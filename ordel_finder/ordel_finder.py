6af739bdae1da18c983ce0d9e5835dca295c3002
b_t1ma <morgan.a9904@gmail.com>
2022-01-29 17:55:55 +0100

    able to find words not in dictionary

--- ordel_finder/ordel_finder.py
+++ ordel_finder/ordel_finder.py
@@ -7,6 +7,9 @@ import smtplib
 import datetime
 import os
 from email import message
+import itertools
+
+SWE_ALPH = "abcdefghijklmnopqrstuvwxyzåäö"
 
 
 class InvalidWordException(Exception):
@@ -33,28 +36,64 @@ def delete_all_except_condition(list_p: list[str], condition: Callable[[str], bo
 class WordReducer:
     def __init__(self, dictionary):
         self.__words = dictionary
-        self.__tested_words = []
-        self.__known = [False] * 5
-        self.__found_letter = []
+        self.__known = [None] * 5
+        self.__found_letter = {}
+        self.__bad_letters = []
 
     def make_random_attempt(self):
         response = []
         while True:
+            if not self.__words:
+                raise WordNotFoundException()
             index = random.randint(0, len(self.__words) - 1)
             word = self.__words[index]
+            self.__words.remove(word)
             try:
-                self.__words.remove(word)
                 response = self.__ask_if_correct(word)
                 if all(i == 1 for i in response):
                     raise WordFound(word)
                 break
             except InvalidWordException:
-                if not self.__words:
-                    break
-        if not self.__words:
-            raise WordNotFoundException()
+                pass
         self.__reduce_words(word, response)
         return word, response
+
+    def expand_words_unconstrained(self):
+        swe_alph_left = [l for l in SWE_ALPH if l not in self.__bad_letters]
+        letter_cols = []
+        banned_letters = []
+
+        # Step 1: Generate all possible search lists
+        for i in range(5):
+            if self.__known[i]:
+                letter_cols.append(self.__known[i])
+                banned_letters.append(None)
+            else:
+                letter_cols.append(swe_alph_left)
+
+        # Step 2: Generate all combinations of letters
+        def comb(i):
+            if i == 5:
+                return []
+            results = []
+            if letter_cols[i] is str:
+                other_results = comb(i + 1)
+                if len(other_results) > 0:
+                    for r in other_results:
+                        results.append(letter_cols[i] + r)
+                else:
+                    results.append(letter_cols[i])
+            else:
+                for l in letter_cols[i]:
+                    other_results = comb(i + 1)
+                    if len(other_results) > 0:
+                        for ll in other_results:
+                            results.append(l + ll)
+                    else:
+                        results.append(l)
+            return results
+
+        self.__words = comb(0)
 
     def __reduce_words(self, word, response):
         for i, valid in enumerate(response):
@@ -70,16 +109,21 @@ class WordReducer:
             self.__words = delete_all_except_condition(
                 self.__words, lambda x: x[index] == word[index]
             )
-            self.__known[index] = True
+            self.__known[index] = word[index]
 
     def __correct_letter_wrong_position(self, word, index):
+        self.__words = delete_all_except_condition(
+            self.__words, lambda x: word[index] in x and x[index] != word[index]
+        )
         if word[index] not in self.__found_letter:
-            self.__words = delete_all_except_condition(
-                self.__words, lambda x: word[index] in x
-            )
-            self.__found_letter.append(word[index])
+            self.__found_letter[word[index]] = [index]
+        else:
+            if index not in self.__found_letter[word[index]]:
+                self.__found_letter[word[index]].append(index)
 
     def __letter_not_in_word(self, word, index, response):
+        if word[index] not in self.__bad_letters:
+            self.__bad_letters.append(word[index])
         count = 0
         for j in range(5):
             if word[index] == word[j] and response[j] == -1:
@@ -126,7 +170,9 @@ def send_email(word, attempts, attempted_words, responses=[], error=False):
             word, attempts
         )
         if not error
-        else "Unable to find todays word. The following attempts were made:"
+        else 'Unable to find todays word by looking in the dictionary. The word was found to be "{}" from brute force. The following attempts were made:'.format(
+            word
+        )
     )
     for i, word in enumerate(attempted_words[:-1]):
         body += "\n\t{}. {} {}".format(i + 1, word, responses[i] if responses else "")
@@ -167,15 +213,26 @@ def ordel_finder():
         if os.getenv("ENABLE_EMAIL") == "1":
             send_email(e.word, len(attempted_words), attempted_words)
     except WordNotFoundException:
-        print("Todays word was not found.")
+        print("Todays word was not found.\nGenerating random unconstrained words")
+        word_reducer.expand_words_unconstrained()
 
-        if os.getenv("ENABLE_EMAIL") == "1":
-            send_email(
-                "None",
-                len(attempted_words),
-                attempted_words,
-                attempted_responses,
-                error=True,
-            )
+        try:
+            while True:
+                word, response = word_reducer.make_random_attempt()
+                print("{}\t{}".format(word, response), flush=True)
+                attempted_words.append(word)
+                attempted_responses.append(response)
+                time.sleep(0.5)
+        except WordFound as e:
+            print("Word is: {}".format(e.word), flush=True)
+
+            if os.getenv("ENABLE_EMAIL") == "1":
+                send_email(
+                    e.word,
+                    len(attempted_words),
+                    attempted_words,
+                    attempted_responses,
+                    error=True,
+                )
     finally:
         print("Done")
